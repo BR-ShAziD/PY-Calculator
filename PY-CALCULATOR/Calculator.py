9570430ab9dbd3049fae83b78f5231ca17bcec2d
BR-ShAziD <brshazid@gmail.com>
2023-04-10 16:51:24 +0600

    Final Commit

---
+++ PY-CALCULATOR/Calculator.py
@@ -0,0 +1,20 @@
+# Taking the Numbers which are going to be calculate:
+
+num1 = int(input("Enter the First Number:\n"))
+num2 = int(input("Enter the Second Number:\n"))
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+exponential = num1 ** num2
+floorDivision = num1 // num2
+Modulas  = num1 % num2
+print("\n")
+print("Addition is:", addition)
+print("Subtraction is:",subtraction)
+print("Multiplication is:",multiplication)
+print("Division is:",division)
+print("Exponential is:",exponential)
+print("Floor Division is:",floorDivision)
+print("Modulas Part is:",Modulas )

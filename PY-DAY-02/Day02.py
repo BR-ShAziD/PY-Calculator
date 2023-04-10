e7b0c0a5e1cac2eafe3fc4801737e38667035d0f
BR-ShAziD <brshazid@gmail.com>
2023-04-10 16:32:49 +0600

    initital commit

---
+++ PY-DAY-02/Day02.py
@@ -0,0 +1,8 @@
+#  If we use the statements without the parenthesis we will get an error cause inside the parenthesis we need to insert an object that can be any sort of data such as integer, string, boolean and so on.
+
+# --- For Printing any obejct of sentecnce or string or sequence of the charecters we need to put the sequence inside the double qoute---
+
+
+print("hello World")
+print(5)
+print(10*2)
